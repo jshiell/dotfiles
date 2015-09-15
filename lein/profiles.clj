460f4a1781ddb6f854e90aac623dd7e7be58a015
James Shiell <james@infernus.org>
2015-09-15 21:41:37 +0100

    Added namespace tools to lein profiles; updated versions

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,8 @@
-{:user {:plugins [[venantius/ultra "0.1.9"]]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
-        :ultra {:color-scheme :solarized_dark}}}
+{:user 
+ {:plugins [[venantius/ultra "0.3.4"]] ; pimp your REPL - https://github.com/venantius/ultra
+  :dependencies [[pjstadig/humane-test-output "0.7.0"] ; better test output - https://github.com/pjstadig/humane-test-output
+                    [org.clojure/tools.namespace "0.2.11"]] ; namespace tools - https://github.com/clojure/tools.namespace
+  :injections [(require 'pjstadig.humane-test-output)
+                  (pjstadig.humane-test-output/activate!)
+                  (require '(clojure.tools.namespace repl find))]
+  :ultra {:color-scheme :solarized_dark}}}
