e06f30246b13086d65aaffb4f32644308a52190b
James Shiell <james@infernus.org>
2018-04-07 13:43:09 +0100

    Removed Lein config

--- lein/profiles.clj
+++
@@ -1,8 +0,0 @@
-{:user 
- {:plugins [[venantius/ultra "0.3.4"]] ; pimp your REPL - https://github.com/venantius/ultra
-  :dependencies [[pjstadig/humane-test-output "0.7.0"] ; better test output - https://github.com/pjstadig/humane-test-output
-                    [org.clojure/tools.namespace "0.2.11"]] ; namespace tools - https://github.com/clojure/tools.namespace
-  :injections [(require 'pjstadig.humane-test-output)
-                  (pjstadig.humane-test-output/activate!)
-                  (require '(clojure.tools.namespace repl find))]
-  :ultra {:color-scheme :solarized_dark}}}
