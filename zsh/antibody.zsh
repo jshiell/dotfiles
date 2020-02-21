39edb80eabbbc855efc451b1b7a9ccdfc3b4cdfa
Jamie Shiell <jamie@infernus.org>
2020-02-21 08:56:33 +0000

    Added non-brew install of Antibody; assume antibody is in /usr/local/bin by default

--- zsh/antibody.zsh
+++ zsh/antibody.zsh
@@ -1,15 +1,26 @@
-if ! antibody -v >/dev/null 2>&1 && brew -v >/dev/null 2>&1; then
-    brew install getantibody/tap/Antibody
+ANTIBODY=/usr/local/bin/antibody
+if ! "$ANTIBODY" -v >/dev/null 2>&1; then
+    if antibody -v  >/dev/null 2>&1; then
+        ANTIBODY=antibody
+    fi
+
+    if ! "$ANTIBODY" -v >/dev/null 2>&1; then
+        if brew -v >/dev/null 2>&1; then
+            brew install getantibody/tap/Antibody
+        else
+            curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
+        fi
+    fi
 fi
 
 ANTIBODY_PLUGINS_TEXT="$_ZSH_CONFIG_DIR/antibody_plugins.txt"
 ANTIBODY_PLUGINS_SH="$_ZSH_CONFIG_DIR/antibody_plugins.sh"
 
-if antibody -v >/dev/null 2>&1; then
+if "$ANTIBODY" -v >/dev/null 2>&1; then
     if [[ -f "$ANTIBODY_PLUGINS_SH" && "$ANTIBODY_PLUGINS_SH" -nt "$ANTIBODY_PLUGINS_TEXT" ]]; then
         source "$ANTIBODY_PLUGINS_SH"
     elif [[ -f "$ANTIBODY_PLUGINS_TEXT" ]]; then
-        antibody bundle <"$ANTIBODY_PLUGINS_TEXT" >"$ANTIBODY_PLUGINS_SH"
+        "$ANTIBODY" bundle <"$ANTIBODY_PLUGINS_TEXT" >"$ANTIBODY_PLUGINS_SH"
         source "$ANTIBODY_PLUGINS_SH"
     else
         echo "No $ANTIBODY_PLUGINS_TEXT found; antibody will not be loaded"
