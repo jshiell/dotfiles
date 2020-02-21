a0144f1b609c7dc0a3f942aab08055d15b94e9da
Jamie Shiell <jamie@infernus.org>
2020-02-21 09:08:21 +0000

    Only add JAVA_HOME to path if set

--- zsh/java.zsh
+++ zsh/java.zsh
@@ -2,7 +2,8 @@ if [[ -x '/usr/libexec/java_home' ]]; then
     export JAVA_HOME=$(/usr/libexec/java_home -v '11.*')
     export JDK_HOME=$JAVA_HOME
 fi
-export PATH=$JAVA_HOME/bin:$PATH
+
+[[ -n "$JAVA_HOME" ]] && export PATH=$JAVA_HOME/bin:$PATH
 
 if [[ -d '/usr/local/android-sdk-macosx' ]]; then
     export ANDROID_HOME=/usr/local/android-sdk-macosx
