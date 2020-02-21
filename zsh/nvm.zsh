9ad2496f247c95db8bef80935437b3a10b38fe88
Jamie Shiell <jamie@infernus.org>
2020-02-21 08:47:04 +0000

    Changed nvm to lazy-loading, which saves ~1.5s on shell startup time

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,12 +1,20 @@
 if [[ -f /usr/local/opt/nvm/nvm.sh ]]; then
-    export NVM_DIR="$HOME/.nvm"
-
-    if [[ ! -d "$NVM_DIR" ]]; then
-        mkdir "$NVM_DIR"
-    fi
-
-    . "/usr/local/opt/nvm/nvm.sh"
+    NVM_HOME_DIR="/usr/local/opt/nvm"
 elif [[ -f $HOME/.nvm/nvm.sh ]]; then
+    NVM_HOME_DIR="$HOME/.nvm"
+fi
+
+if [[ -n "$NVM_HOME_DIR" ]]; then
     export NVM_DIR="$HOME/.nvm"
-    . "$HOME/.nvm/nvm.sh"
+    [[ ! -d "$NVM_DIR" ]] && mkdir "$NVM_DIR"
+
+    # all credit to https://www.growingwiththeweb.com/2018/01/slow-nvm-init.html
+    declare -a __NODE_COMMANDS=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
+    function __init_nvm() {
+        for i in "${__NODE_COMMANDS[@]}"; do unalias $i; done
+        . "$NVM_HOME_DIR/nvm.sh"
+        unset __NODE_COMMANDS
+        unset -f __init_nvm
+    }
+    for i in "${__NODE_COMMANDS[@]}"; do alias $i='__init_nvm && '$i; done
 fi
