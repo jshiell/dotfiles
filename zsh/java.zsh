f086e554e3b15eaa2305090df14f26a7f8541267
Jamie Shiell <jamie@infernus.org>
2020-03-23 10:28:59 +0000

    Added function to switch java version in play

--- zsh/java.zsh
+++ zsh/java.zsh
@@ -1,9 +1,41 @@
 if [[ -x '/usr/libexec/java_home' ]]; then
-    export JAVA_HOME=$(/usr/libexec/java_home -v '11.*')
-    export JDK_HOME=$JAVA_HOME
-fi
+    _path_remove() {
+        local DIR_TO_REMOVE="$1"
+        local NEW_PATH
+        local CURRENT_DIR
+        for CURRENT_DIR in $(echo $PATH | sed -e 's/:/ /g'); do
+            if [[ "$CURRENT_DIR" != "$DIR_TO_REMOVE" ]]; then
+                if [[ -z "$NEW_PATH" ]]; then
+                    NEW_PATH=$CURRENT_DIR
+                else
+                    NEW_PATH="${NEW_PATH}:${CURRENT_DIR}"
+                fi
+            fi
+        done
+        export PATH="$NEW_PATH"
+    }
 
-[[ -n "$JAVA_HOME" ]] && export PATH=$JAVA_HOME/bin:$PATH
+    _change_jdk() {
+        local JDK_VERSION=$1
+
+        local JAVA_VERSION_HOME
+        JAVA_VERSION_HOME=$(/usr/libexec/java_home -v $JDK_VERSION)
+        if [[ $? -ne 0 ]]; then
+            echo "Couldn't find JDK for version $JDK_VERSION"
+            return 1
+        fi
+
+        [[ -n "$JAVA_HOME" ]] && _path_remove "$JAVA_HOME/bin"
+
+        export JAVA_HOME=$JAVA_VERSION_HOME
+        export JDK_HOME=$JAVA_HOME
+        export PATH="$JAVA_HOME/bin:$PATH"    
+    }
+
+    _change_jdk 11
+
+    alias use-java='_change_jdk $1'
+fi
 
 if [[ -d '/usr/local/android-sdk-macosx' ]]; then
     export ANDROID_HOME=/usr/local/android-sdk-macosx
