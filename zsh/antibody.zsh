ae95a58f3332eb13835581be19122de608a350a0
Jamie Shiell <jamie@infernus.org>
2020-03-24 10:18:57 +0000

    Ensure we ssh-add before calling Antibody bundle, for Antibody >= 5.0

--- zsh/antibody.zsh
+++ zsh/antibody.zsh
@@ -22,6 +22,7 @@ if "$ANTIBODY" -v >/dev/null 2>&1; then
     if [[ -f "$ANTIBODY_PLUGINS_SH" && "$ANTIBODY_PLUGINS_SH" -nt "$ANTIBODY_PLUGINS_TEXT" ]]; then
         source "$ANTIBODY_PLUGINS_SH"
     elif [[ -f "$ANTIBODY_PLUGINS_TEXT" ]]; then
+        ssh-add
         "$ANTIBODY" bundle <"$ANTIBODY_PLUGINS_TEXT" >"$ANTIBODY_PLUGINS_SH"
         source "$ANTIBODY_PLUGINS_SH"
     else
