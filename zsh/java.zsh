3c1129ba7ed7b4f9b30f68cdb89c7a264cb2a665
Jamie Shiell <jamie@infernus.org>
2020-03-30 09:01:19 +0100

    Changed default JDK to 14

--- zsh/java.zsh
+++ zsh/java.zsh
@@ -32,7 +32,7 @@ if [[ -x '/usr/libexec/java_home' ]]; then
         export PATH="$JAVA_HOME/bin:$PATH"    
     }
 
-    _change_jdk 11
+    _change_jdk 14
 
     alias use-java='_change_jdk $1'
 fi
