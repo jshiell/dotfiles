d6a28d26999eb7b29bd364e82f95759d15cff1ff
Jamie Shiell <jamie@infernus.org>
2020-07-10 12:57:50 +0100

    Added fuzzier completion logic

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -54,3 +54,4 @@ zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
 
 zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
