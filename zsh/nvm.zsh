33d32e4dd8fc7dc0b73db04fb6871854ab95d02d
Jamie Shiell <jamie@infernus.org>
2020-11-16 10:49:39 +0000

    Removed lazy-load of nvm, as it breaks too many scripts that depend on npm

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -7,14 +7,5 @@ fi
 if [[ -n "$NVM_HOME_DIR" ]]; then
     export NVM_DIR="$HOME/.nvm"
     [[ ! -d "$NVM_DIR" ]] && mkdir "$NVM_DIR"
-
-    # all credit to https://www.growingwiththeweb.com/2018/01/slow-nvm-init.html
-    declare -a __NODE_COMMANDS=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
-    function __init_nvm() {
-        for i in "${__NODE_COMMANDS[@]}"; do unalias $i; done
-        . "$NVM_HOME_DIR/nvm.sh"
-        unset __NODE_COMMANDS
-        unset -f __init_nvm
-    }
-    for i in "${__NODE_COMMANDS[@]}"; do alias $i='__init_nvm && '$i; done
+    . "$NVM_HOME_DIR/nvm.sh"
 fi
