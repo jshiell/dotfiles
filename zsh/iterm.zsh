4818d7cbb982844b57a53cf2f19365f5e06aad30
Jamie Shiell <jamie@infernus.org>
2021-11-12 13:45:35 +0000

    Dropped iterm profile change for SSH

--- zsh/iterm.zsh
+++ zsh/iterm.zsh
@@ -1,13 +1,3 @@
 if [[ -f "$HOME/.iterm2_shell_integration.zsh" ]]; then
 	source "$HOME/.iterm2_shell_integration.zsh"
 fi
-
-ssh_cleanup() {
-    echo -e "\033]50;SetProfile=Default\a"
-}
-
-ssh() {
-    trap ssh_cleanup EXIT
-    echo -e "\033]50;SetProfile=SSH\a"
-    /usr/bin/ssh $@
-}
