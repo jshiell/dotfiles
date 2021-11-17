1f201aee61d46cf2519c160b44d45263021818ee
Jamie Shiell <jamie@infernus.org>
2021-11-17 11:21:02 +0000

    Reset iTerm title after ssh session

--- zsh/iterm.zsh
+++ zsh/iterm.zsh
@@ -1,3 +1,10 @@
-if [[ -f "$HOME/.iterm2_shell_integration.zsh" ]]; then
-	source "$HOME/.iterm2_shell_integration.zsh"
+if [[ "$TERM_PROGRAM" == 'iTerm.app' ]]; then
+	if [[ -f "$HOME/.iterm2_shell_integration.zsh" ]]; then
+		source "$HOME/.iterm2_shell_integration.zsh"
+	fi
+
+	ssh() {
+		/usr/bin/ssh "$@"
+		echo -ne "\033]0;$*\007"
+	}
 fi
