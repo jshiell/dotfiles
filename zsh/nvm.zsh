099ff72a160a099dc8cdcf5009381f293aa07fae
Jamie Shiell <jamie@infernus.org>
2021-12-07 09:40:05 +0000

    Lazy-load NVM to improve startup perf

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,13 +1,33 @@
-if [[ -f /usr/local/opt/nvm/nvm.sh ]]; then
-    NVM_HOME_DIR="/usr/local/opt/nvm"
-elif [[ -f $HOME/.nvm/nvm.sh ]]; then
-    NVM_HOME_DIR="$HOME/.nvm"
-fi
+lazy_load_nvm() {
+    unset -f node
+    unset -f npm
+    unset -f nvm
+    if [[ -f /usr/local/opt/nvm/nvm.sh ]]; then
+        NVM_HOME_DIR="/usr/local/opt/nvm"
+    elif [[ -f $HOME/.nvm/nvm.sh ]]; then
+        NVM_HOME_DIR="$HOME/.nvm"
+    fi
 
-if [[ -n "$NVM_HOME_DIR" ]]; then
-    export NVM_DIR="$HOME/.nvm"
-    [[ ! -d "$NVM_DIR" ]] && mkdir "$NVM_DIR"
-    setopt no_aliases
-    . "$NVM_HOME_DIR/nvm.sh"
-    setopt aliases
-fi
+    if [[ -n "$NVM_HOME_DIR" ]]; then
+        export NVM_DIR="$HOME/.nvm"
+        [[ ! -d "$NVM_DIR" ]] && mkdir "$NVM_DIR"
+        setopt no_aliases
+        . "$NVM_HOME_DIR/nvm.sh"
+        setopt aliases
+    fi
+}
+
+node() {
+    lazy_load_nvm
+    node $@
+}
+
+npm() {
+    lazy_load_nvm
+    npm $@
+}
+
+nvm() {
+    lazy_load_nvm
+    nvm $@
+}
