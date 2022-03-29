bdf5308ffd6d8dfbb6d76de52aeffd68266b4335
Jamie Shiell <james.shiell@springer.com>
2022-03-29 13:09:04 +0100

    Fixed imagemagick path on mac/aarch64

--- zsh/brew.zsh
+++ zsh/brew.zsh
@@ -2,18 +2,18 @@ if [[ -d /opt/X11/bin ]]; then
     export PATH="/opt/X11/bin:$PATH"
 fi
 
+HOMEBREW_BASE_PATH="/usr/local"
 if [[ "$(uname -p)" == 'arm' ]]; then
-    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-else
-    export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+    HOMEBREW_BASE_PATH="/opt/homebrew"
 fi
+export PATH="$HOMEBREW_BASE_PATH/bin:$HOMEBREW_BASE_PATH/sbin:$PATH"
 
-if [[ -d "/usr/local/opt/imagemagick@6" ]]; then
-    export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-    export LDFLAGS="$LDFLAGS -L/usr/local/opt/imagemagick@6/lib"
-    export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/imagemagick@6/include"
-    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/imagemagick@6/lib/pkgconfig"
-    export MAGICK_HOME="/usr/local/opt/imagemagick@6"
+if [[ -d "$HOMEBREW_BASE_PATH/opt/imagemagick@6" ]]; then
+    export PATH="$HOMEBREW_BASE_PATH/opt/imagemagick@6/bin:$PATH"
+    export LDFLAGS="$LDFLAGS -L$HOMEBREW_BASE_PATH/opt/imagemagick@6/lib"
+    export CPPFLAGS="$CPPFLAGS -I$HOMEBREW_BASE_PATH/opt/imagemagick@6/include"
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOMEBREW_BASE_PATH/opt/imagemagick@6/lib/pkgconfig"
+    export MAGICK_HOME="$HOMEBREW_BASE_PATH/opt/imagemagick@6"
 fi
 
 if bat --version >/dev/null 2>&1; then
