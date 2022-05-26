18890a5037763b2753a646fd638b2d298e3c7964
Jamie Shiell <james.shiell@springer.com>
2022-05-26 09:27:40 +0100

    Init jenv if present

--- zsh/java.zsh
+++ zsh/java.zsh
@@ -40,3 +40,7 @@ fi
 if [[ -d '/usr/local/android-sdk-macosx' ]]; then
     export ANDROID_HOME=/usr/local/android-sdk-macosx
 fi
+
+if jenv --version >/dev/null 2>&1; then
+    eval "$(jenv init -)"
+fi
