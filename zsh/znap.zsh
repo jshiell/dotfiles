19d4243b199db61edb7c3bc4dada1f53810a9240
James Shiell <james.shiell@springer.com>
2025-01-03 15:35:58 +0000

    Change zsnap to use SSH pull by default to avoid completions bug

--- zsh/znap.zsh
+++ zsh/znap.zsh
@@ -3,6 +3,7 @@ ZNAP_HOME="$HOME/.znap"
 mkdir -p "$ZNAP_HOME"
 
 zstyle ':znap:*' repos-dir "$ZNAP_HOME"
+zstyle ':znap:clone:*' default-server "git@github.com:"
 
 if [[ ! -d "$ZNAP_HOME/zsh-snap" ]]; then
     git clone --depth 1 https://github.com/marlonrichert/zsh-snap.git --branch "$ZNAP_RELEASE" "$ZNAP_HOME/zsh-snap"
@@ -19,9 +20,9 @@ fi
 znap source ohmyzsh/ohmyzsh plugins/{common-aliases,colored-man-pages,git}
 
 znap source zsh-users/zsh-syntax-highlighting
-znap source zsh-users/zsh-completions
 znap source zsh-users/zsh-history-substring-search
+znap source zsh-users/zsh-completions
 
 if profiles help >/dev/null 2>&1 && profiles show | grep SpringerNature >/dev/null 2>&1; then
-    znap source git@github.com:springernature/sn-zsh-extensions
+    znap source springernature/sn-zsh-extensions
 fi
