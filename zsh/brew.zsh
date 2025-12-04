c1e91c79c9295b58b3e60b9996cf09408617859a
Jamie Shiell <jamie@infernus.org>
2025-12-04 20:53:01 +0000

    Support linuxbrew e.g. on Bazzite

--- zsh/brew.zsh
+++ zsh/brew.zsh
@@ -2,26 +2,33 @@ if [[ -d /opt/X11/bin ]]; then
     export PATH="/opt/X11/bin:$PATH"
 fi
 
-HOMEBREW_BASE_PATH="/usr/local"
-if [[ "$(uname -p)" == 'arm' ]]; then
-    HOMEBREW_BASE_PATH="/opt/homebrew"
-fi
-export PATH="$HOMEBREW_BASE_PATH/bin:$HOMEBREW_BASE_PATH/sbin:$PATH"
-
-if [[ -d "$HOMEBREW_BASE_PATH/opt/imagemagick@6" ]]; then
-    export PATH="$HOMEBREW_BASE_PATH/opt/imagemagick@6/bin:$PATH"
-    export LDFLAGS="$LDFLAGS -L$HOMEBREW_BASE_PATH/opt/imagemagick@6/lib"
-    export CPPFLAGS="$CPPFLAGS -I$HOMEBREW_BASE_PATH/opt/imagemagick@6/include"
-    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOMEBREW_BASE_PATH/opt/imagemagick@6/lib/pkgconfig"
-    export MAGICK_HOME="$HOMEBREW_BASE_PATH/opt/imagemagick@6"
+if [[ "$(uname -s)" == 'Linux' && -d /home/linuxbrew/.linuxbrew ]]; then
+    HOMEBREW_BASE_PATH="/home/linuxbrew/.linuxbrew"
+else
+    HOMEBREW_BASE_PATH="/usr/local"
+    if [[ "$(uname -p)" == 'arm' ]]; then
+        HOMEBREW_BASE_PATH="/opt/homebrew"
+    fi
 fi
 
-if bat --version >/dev/null 2>&1; then
-    alias cat=bat
-fi
+if [[ -x "$HOMEBREW_BASE_PATH/bin/brew" ]]; then
+    export PATH="$HOMEBREW_BASE_PATH/bin:$HOMEBREW_BASE_PATH/sbin:$PATH"
 
-if prettyping --help >/dev/null 2>&1; then
-    alias ping='prettyping --nolegend'
-fi
+    if [[ -d "$HOMEBREW_BASE_PATH/opt/imagemagick@6" ]]; then
+        export PATH="$HOMEBREW_BASE_PATH/opt/imagemagick@6/bin:$PATH"
+        export LDFLAGS="$LDFLAGS -L$HOMEBREW_BASE_PATH/opt/imagemagick@6/lib"
+        export CPPFLAGS="$CPPFLAGS -I$HOMEBREW_BASE_PATH/opt/imagemagick@6/include"
+        export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOMEBREW_BASE_PATH/opt/imagemagick@6/lib/pkgconfig"
+        export MAGICK_HOME="$HOMEBREW_BASE_PATH/opt/imagemagick@6"
+    fi
 
-export HOMEBREW_CASK_OPTS="--appdir=/Applications --prefpanedir=/Library/PreferencePanes"
+    if bat --version >/dev/null 2>&1; then
+        alias cat=bat
+    fi
+
+    if prettyping --help >/dev/null 2>&1; then
+        alias ping='prettyping --nolegend'
+    fi
+
+    export HOMEBREW_CASK_OPTS="--appdir=/Applications --prefpanedir=/Library/PreferencePanes"
+fi
