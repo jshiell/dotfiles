6322bfda6e1b01fbf693a8099e4cb3af62fcc699
Jamie Shiell <jamie@infernus.org>
2026-02-21 22:14:43 +0000

    Remove jenv support in favour of mise

--- zsh/java.zsh
+++ zsh/java.zsh
@@ -1,7 +1,3 @@
 if [[ -d '/usr/local/android-sdk-macosx' ]]; then
     export ANDROID_HOME=/usr/local/android-sdk-macosx
 fi
-
-if jenv --version >/dev/null 2>&1; then
-    eval "$(jenv init -)"
-fi
